060697bc0527856b38241a0ee6869d48c6107ccd
arifulmrislam <ariful.mr.islam@gmail.com>
2024-01-09 12:18:39 +0600

    Module 0 JS revise

--- Reactive Accelerator/Module 0 Introduction to course/0.11 JavaScript Refresher/script.js
+++ Reactive Accelerator/Module 0 Introduction to course/0.11 JavaScript Refresher/script.js
@@ -17,3 +17,109 @@ function learn (topic) {
 learn("javascript");
 
 console.log(lang); */
+
+// Array Methods
+
+// const arr = ['Mango', 'Banana', 'Pineapple', 'Orange', 'Apple', 'Watermelon'];
+// const numbers = [1, 2, 3, 4, 5];
+// const str = "Hello";
+// const find = arr.find((f)=> f === "Orange");
+// const find = arr.findIndex((f)=> f === "Watermelon");
+// const find = numbers.filter((f)=> f % 2 == 0);
+// const find = arr.slice(2,4);
+// const find = arr.splice(5, 0, 'fruits-1', 'fruits-2');
+// const find = numbers.map((f) => f * 2);
+// const find = numbers.reduce((total, num) => total + num);
+// const find = arr.forEach((f) => console.log(f));
+// const find = str.reverse();
+// const find = str.split('').reverse().join('');
+// for(let i=str.length-1;i>=0;i--){
+//     var newRev = str[i];
+// console.log(`${newRev}`);
+// }
+// console.log(find);
+// console.log(numbers);
+
+//Object
+
+// const obj = {
+//     name: "Ariful",
+//     age: 29,
+//     education:
+//     {
+//         school:"High School",
+//         certificate: "SSC 4.94"
+//     }
+// }
+
+// console.log(Object.keys(obj.education));
+// console.log(Object.values(obj));
+// const {name, age} = obj;
+
+// console.log(name, age);
+
+// const dogFactory = (name, age, breed) => {
+//     return {
+//         name: name,
+//         age: age,
+//         breed: breed,
+//         bark() {
+//             console.log("Woof!");
+//         },
+//     };
+// };
+
+// const myDog = dogFactory("Tiger",3,"German Shafer")
+// myDog.bark();
+
+// console.log(Object.values(myDog));
+
+// const cat = {
+//     name: 'Pipey',
+//     age: 8,
+//     whatName(){
+//         return this.name;
+//     }
+// };
+// console.log(cat.whatName());
+
+// const engine = {
+//     start(adverb){
+//         console.log(`The engine starts up ${adverb}....`);
+//     },
+
+//     sputter:() =>{
+//         console.log(`The engine sputters...`);
+//     },
+// };
+
+// console.log(engine.start('noisily'));
+// console.log(engine.sputter);
+
+//spread
+
+// const arr = ["Mango", "Banana", "Pineapple", "Orange", "Apple", "Watermelon"];
+// const arr1 = [...arr];
+
+// function arr2 (...rest){
+//     return rest.reduce((arr2, cur) => arr2+cur,0);
+// }
+// console.log(arr1);
+// console.log(arr2);
+
+//Condition
+
+// var c = "January";
+
+// switch(c){
+//     case 'January':
+//         console.log("It's 1st month of the year");
+//         break;
+
+//     case 'February':
+//         console.log("Nothing");
+//         break;
+//     case 'March':
+//         console.log('Hello')
+//         break;
+// }
