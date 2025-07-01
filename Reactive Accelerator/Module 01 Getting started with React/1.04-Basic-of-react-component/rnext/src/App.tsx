59b5ddfe8c0ae3737638813af94a3329b52cf11b
arifulmrislam <ariful.mr.islam@gmail.com>
2025-06-30 21:27:26 -0500

    module 1

--- Reactive Accelerator/Module 01 Getting started with React/1.04-Basic-of-react-component/rnext/src/App.tsx
+++ Reactive Accelerator/Module 01 Getting started with React/1.04-Basic-of-react-component/rnext/src/App.tsx
@@ -1,13 +1,42 @@
 import './App.css';
 import PackingList from './Packing';
 
-
+const people = [{
+    id: 0,
+    name: 'Creola Katherine Johnson',
+    profession: 'mathematician',
+}, {
+        id: 1,
+        name: 'Mario José Molina-Pasquel Henríquez',
+        profession: 'chemist',
+    }, {
+        id: 2,
+        name: 'Mohammad Abdus Salam',
+        profession: 'physicist',
+    }, {
+        id: 3,
+        name: 'Percy Lavon Julian',
+        profession: 'chemist',
+    }, {
+        id: 4,
+        name: 'Subrahmanyan Chandrasekhar',
+        profession: 'astrophysicist',
+    }];
 
 
 function App() {
+    //method chaining
+    // const chemists = people.filter(person => person.profession === 'chemist').chemists.map(chemist => <li>{chemist.name}</li>);
+
+    const chemists = people.filter(person => person.profession === 'chemist' );
+
+    const listItems = chemists.map(chemist => <li key={chemist.id}>{chemist.name}</li>);  
+
     return (
         <>
-            <PackingList/>
+        <div>
+            {listItems}
+        </div>
         </>
     )
 }
